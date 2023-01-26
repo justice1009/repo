10709aa6992cd9736be71967887ca58d59dfcf75
justice1009 <117140465+justice1009@users.noreply.github.com>
2023-01-26 10:54:15 +0900

    Update KPI2023.py

--- KPI2023.py
+++ KPI2023.py
@@ -33,7 +33,7 @@ if name == '품질경영':
         if option == '3분기':
             def main() :    
                 # pd.set_option('display.max.columns', 200)
-                df= pd.read_excel("KPI2022.xlsx", sheet_name="KPI실적모니터링") # CSV 파일 불러오고 df 변수에 저장        
+                df= pd.read_excel("KPI2022.xlsx", sheet_name="3분기") # CSV 파일 불러오고 df 변수에 저장        
                 ds= df.fillna('')        
                 # ds.set_index('Part', inplace=True)
                 ds.style.set_properties(color="black", align="right")  
@@ -65,3 +65,98 @@ if name == '품질경영':
 
             if __name__ == '__main__' :
                 main()  
+
+        if option == '1분기':
+            def main() :    
+                # pd.set_option('display.max.columns', 200)
+                df= pd.read_excel("KPI2022.xlsx", sheet_name="1분기") # CSV 파일 불러오고 df 변수에 저장        
+                ds= df.fillna('')        
+                # ds.set_index('Part', inplace=True)
+                ds.style.set_properties(color="black", align="right")                  
+
+                def draw_color_cell(x,color):
+                    color = f'background-color:{color}'
+                    return color
+                
+                ds= ds.style.applymap(draw_color_cell,color="#FFFAF0",subset=pd.IndexSlice[:,['1분기 목표','21년 1분기 누계 실적']]).set_precision(2) 
+                st.dataframe(ds, width=2500,height=420)     
+
+                def convert_df(df):
+                # IMPORTANT: Cache the conversion to prevent computation on every rerun
+                    return df.to_csv().encode('CP949')
+
+                csv = convert_df(df)
+                st.download_button(
+                label="Download data as CSV",
+                data=csv,
+                file_name='Painpoint_dx.csv',
+                mime='text/csv')                                   
+
+                st.success("🎈 KPI 실적 모니터링 결과가 조회되었습니다.")                   
+
+            if __name__ == '__main__' :
+                main()  
+
+        if option == '2분기':
+            def main() :    
+                # pd.set_option('display.max.columns', 200)
+                df= pd.read_excel("KPI2022.xlsx", sheet_name="2분기") # CSV 파일 불러오고 df 변수에 저장        
+                ds= df.fillna('')        
+                # ds.set_index('Part', inplace=True)
+                ds.style.set_properties(color="black", align="right")                  
+
+                def draw_color_cell(x,color):
+                    color = f'background-color:{color}'
+                    return color
+                
+                ds= ds.style.applymap(draw_color_cell,color="#FFFAF0",subset=pd.IndexSlice[:,['2분기 목표','21년 2분기 누계 실적']]).set_precision(2) 
+                st.dataframe(ds, width=2500,height=420)     
+
+                def convert_df(df):
+                # IMPORTANT: Cache the conversion to prevent computation on every rerun
+                    return df.to_csv().encode('CP949')
+
+                csv = convert_df(df)
+                st.download_button(
+                label="Download data as CSV",
+                data=csv,
+                file_name='Painpoint_dx.csv',
+                mime='text/csv')                                   
+
+                st.success("🎈 KPI 실적 모니터링 결과가 조회되었습니다.")                   
+
+            if __name__ == '__main__' :
+                main()  
+
+
+        if option == '4분기':
+            def main() :    
+                # pd.set_option('display.max.columns', 200)
+                df= pd.read_excel("KPI2022.xlsx", sheet_name="4분기") # CSV 파일 불러오고 df 변수에 저장        
+                ds= df.fillna('')        
+                # ds.set_index('Part', inplace=True)
+                ds.style.set_properties(color="black", align="right")                  
+
+                def draw_color_cell(x,color):
+                    color = f'background-color:{color}'
+                    return color
+                
+                ds= ds.style.applymap(draw_color_cell,color="#FFFAF0",subset=pd.IndexSlice[:,['4분기 목표','21년 4분기 누계 실적']]).set_precision(2) 
+                st.dataframe(ds, width=2500,height=420)     
+
+                def convert_df(df):
+                # IMPORTANT: Cache the conversion to prevent computation on every rerun
+                    return df.to_csv().encode('CP949')
+
+                csv = convert_df(df)
+                st.download_button(
+                label="Download data as CSV",
+                data=csv,
+                file_name='Painpoint_dx.csv',
+                mime='text/csv')                                   
+
+                st.success("🎈 KPI 실적 모니터링 결과가 조회되었습니다.")                   
+
+            if __name__ == '__main__' :
+                main()  
+
